350a583759f07e6a9e3ef95978c4fe4c57bfe7e2
xingzheng <xingzheng@hotmail.com>
2022-02-16 19:03:44 +0800

    Add project files.

---
+++ FileReadWriteSample/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "TestFile.txt");
+var fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
+StreamWriter sw = new StreamWriter(fileStream);
+int cout = 0;
+while (true)
+{
+    for (var i = 0; i < 10; i++)
+    {
+        sw.WriteLine(cout++);
+    }
+    Task.Delay(3000);
+}
---
+++ ReadSharedFile/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Try to read someting!");
+
+var path =Path.Combine( Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "TestFile.txt");
+var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read,FileShare.ReadWrite);
+var reader = new StreamReader(fileStream);
+
+while (!reader.EndOfStream)
+{
+    Console.WriteLine(reader.ReadLine());
+    Task.Delay(1000);
+}
